92a7a3ccd937d88478f0bf53008e471463544699
AyanChikode <chikodeayan4@gmail.com>
2026-03-03 23:58:05 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { ProductComponent } from './product/product.component';
-import { CartComponent } from './cart/cart.component';
 import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  {path:"",component:HomeComponent},
-  {path:"about",component:AboutComponent},
-  {path:"contact",component:ContactComponent},
-  {path:"product",component:ProductComponent},
-  {path:"cart", component:CartComponent},
-  {path:"products",component:ProductsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
